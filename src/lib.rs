af8e0d1bedbbea71e17ed61d6e7a041a19990b30
Tomash Ghz <ghz.tomash@hotmail.com>
2024-03-05 17:46:00 +0200

    Make API request

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,27 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use serde::{Deserialize, Serialize};
+use reqwest::Error;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Response {
+    ip: String,
+    ip_decimal: u32,
+    country: String,
+    country_iso: String,
+    country_eu: bool,
+    region_name: String,
+    region_code: String,
+    zip_code: String,
+    city: String,
+    latitude: f32,
+    longtitude: f32,
+    time_zone: String,
+    asn: String,
+    asn_org: String,
+}
+
+fn make_api_request() -> Result<String, Error> {
+    let resp = reqwest::blocking::get("http://ifconfig.co/json")?.text()?;
+    Ok(resp)
 }
 
 #[cfg(test)]
@@ -7,8 +29,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn request() {
+        let result = make_api_request();
+        println!("{:#?}", result);
+        assert!(result.is_ok());
     }
 }
