258234f92b443cf9bb6196c3572c2124ab6bcee3
Tomash Ghz <ghz.tomash@hotmail.com>
2024-03-06 21:33:01 +0200

    Better error handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
-use std::time::{SystemTime, Duration};
+use serde::{Deserialize, Serialize};
 use std::fs::File;
 use std::io::prelude::*;
-use serde::{Deserialize, Serialize};
-use reqwest::{Error};
+use std::time::{Duration, SystemTime};
+use std::error;
 
 const CACHE_TIME: u64 = 2;
+
+type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Response {
@@ -28,8 +30,8 @@ pub struct Response {
 }
 
 impl Response {
-    pub fn parse(input: String) -> Result<Response, Error> {
-        let deserialized: Response = serde_json::from_str(&input).unwrap();
+    pub fn parse(input: String) -> Result<Response> {
+        let deserialized: Response = serde_json::from_str(&input)?;
         Ok(deserialized)
     }
 }
@@ -47,56 +49,60 @@ impl Cache {
     const CACHE_PATH: &'static str = "public-ip-cache.txt";
 
     fn new(response: Response) -> Cache {
-        let cache = Cache{response, response_time: SystemTime::now()};
+        let cache = Cache {
+            response,
+            response_time: SystemTime::now(),
+        };
         cache
     }
 
-    fn save(&self) -> std::io::Result<()> {
-        let serialized = serde_json::to_string(self).unwrap();
+    fn save(&self) -> Result<()> {
+        let serialized = serde_json::to_string(self)?;
         let mut file = File::create(Self::CACHE_PATH)?;
         file.write_all(serialized.as_bytes())?;
         Ok(())
     }
 
-    fn load() -> Option<Cache> {
-        let mut file = File::open(Self::CACHE_PATH).unwrap();
+    fn load() -> Result<Cache> {
+        let mut file = File::open(Self::CACHE_PATH)?;
         let mut contents = String::new();
-        file.read_to_string(&mut contents).unwrap();
-        let deserialized: Cache = serde_json::from_str(&contents).unwrap();
-        Some(deserialized)
+        file.read_to_string(&mut contents)?;
+        let deserialized: Cache = serde_json::from_str(&contents)?;
+        Ok(deserialized)
     }
 }
 
-fn make_api_request() -> Result<String, Error> {
+fn make_api_request() -> Result<String> {
     let response = reqwest::blocking::get("http://ifconfig.co/json")?.text()?;
     Ok(response)
 }
 
-pub fn get_response() -> Option<Response> {
+pub fn get_response() -> Result<Response> {
     let cached = Cache::load();
-    if let Some(cache) = cached {
-        let difference = SystemTime::now().duration_since(cache.response_time).unwrap();
+    if let Ok(cache) = cached {
+        let difference = SystemTime::now()
+            .duration_since(cache.response_time)?;
         println!("Difference: {:?}", difference);
         if difference <= Duration::from_secs(CACHE_TIME) {
             println!("Using cache");
-            return Some(cache.response);
+            return Ok(cache.response);
         }
     }
     println!("Making new request");
     // no chache or it's too old, make a new request.
     if let Ok(result) = make_api_request() {
-        let response = Response::parse(result).unwrap();
+        let response = Response::parse(result)?;
         let cache = Cache::new(response);
-        cache.save().unwrap();
-        return Some(cache.response);
+        cache.save()?;
+        return Ok(cache.response);
     }
-    None
+    Err("Error getting response".into())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    const TEST_INPUT: &str = "{\n \"ip\": \"1.1.1.1\",\n \"ip_decimal\": 16843009,\n}";
+    const TEST_INPUT: &str = "{\n \"ip\": \"1.1.1.1\",\n \"ip_decimal\": 16843009\n}";
 
     #[test]
     fn test_request() {
@@ -120,13 +126,16 @@ mod tests {
         cache.save().unwrap();
         let cached = Cache::load().unwrap();
         assert_eq!(cached.response.ip, "1.1.1.1", "IP address not matching");
-        assert_eq!(cache.response.ip_decimal, cached.response.ip_decimal, "IP address not matching");
+        assert_eq!(
+            cache.response.ip_decimal, cached.response.ip_decimal,
+            "IP address not matching"
+        );
     }
 
     #[test]
     fn test_get_response() {
         let response = get_response();
         println!("{:#?}", response);
-        assert!(response.is_some());
+        assert!(response.is_ok());
     }
 }
