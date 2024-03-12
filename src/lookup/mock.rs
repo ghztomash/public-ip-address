939cf64103f3f14896938ac5875d60c44d5fdaa7
Tomash Ghz <ghz.tomash@hotmail.com>
2024-03-12 02:09:15 +0100

    Add sleep to simulate api call in Mock

--- src/lookup/mock.rs
+++ src/lookup/mock.rs
@@ -1,6 +1,7 @@
 use crate::lookup::LookupService;
 use crate::LookupResponse;
 use crate::Result;
+use std::{thread, time};
 
 pub struct Mock<'a> {
     pub ip: &'a str,
@@ -8,6 +9,8 @@ pub struct Mock<'a> {
 
 impl LookupService for Mock<'_> {
     fn make_api_request(&self) -> Result<String> {
+        // simulate blocking api call
+        thread::sleep(time::Duration::from_millis(100));
         Ok(self.ip.to_string())
     }
     fn parse_reply(&self, json: String) -> Result<LookupResponse> {
