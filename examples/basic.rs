1de05147418f6694d841f400f81e7e291e286414
Tomash Ghz <ghz.tomash@hotmail.com>
2024-03-28 14:00:42 +0100

    Expand basic example

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,7 +1,14 @@
+use public_ip_address::lookup::LookupProvider;
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let result = public_ip_address::perform_lookup()?;
-    println!("{}", result);
+    let result =
+        public_ip_address::perform_cached_lookup_with_list(vec![LookupProvider::MyIp], None)?;
+    println!(
+        "Hello {} from {}, {}.",
+        result.ip,
+        result.city.unwrap_or("unknown".to_string()),
+        result.country.unwrap_or("unknown".to_string())
+    );
     Ok(())
 }
