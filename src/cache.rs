367ffcf51b95f59ecdb5f14ad38826e068e158a7
Tomash Ghz <ghz.tomash@hotmail.com>
2024-03-29 15:14:53 +0100

    Cache module documentation

--- src/cache.rs
+++ src/cache.rs
@@ -1,7 +1,7 @@
-//! # ResponseCache module
+//! # ResponseCache Module
 //!
-//! Holds a single lookup response and the time it was created.
-//! Can be saved and loaded from disk.
+//! It holds a single lookup response and the time it was created. The cached response can be saved and loaded from disk.
+//! It also provides a method to delete the cache from disk.
 //!
 //! ## Example
 //! ```rust
@@ -10,12 +10,14 @@
 //! use public_ip_address::lookup::LookupProvider;
 //!
 //! fn main() -> Result<(), Box<dyn Error>> {
-//!     ResponseCache::new(LookupResponse::new(
+//!     let response_cache = ResponseCache::new(LookupResponse::new(
 //!         "1.1.1.1".to_string(),
 //!         LookupProvider::IpBase,
-//!     )).save()?;
+//!     ));
+//!     response_cache.save()?;
 //!     let cached = ResponseCache::load()?;
 //!     println!("{:?}", cached);
+//!     cached.delete()?;
 //!     Ok(())
 //! }
 //! ```
@@ -37,7 +39,11 @@ pub struct ResponseCache {
 }
 
 impl ResponseCache {
-    /// Create a new ResponseCache
+    /// Creates a new `ResponseCache` instance.
+    ///
+    /// # Arguments
+    ///
+    /// * `response` - A `LookupResponse` to be cached.
     pub fn new(response: LookupResponse) -> ResponseCache {
         ResponseCache {
             response,
@@ -45,7 +51,10 @@ impl ResponseCache {
         }
     }
 
-    /// Save the cache to disk
+    /// Saves the `ResponseCache` instance to disk.
+    ///
+    /// This function serializes the `ResponseCache` instance to a JSON string, encodes it using Base64, and then writes it to a file on disk.
+    /// The file is located at the path returned by the `get_cache_path` function.
     pub fn save(&self) -> Result<()> {
         let serialized = serde_json::to_string(self)?;
         let encoded = BASE64_STANDARD.encode(serialized);
@@ -54,7 +63,7 @@ impl ResponseCache {
         Ok(())
     }
 
-    /// Load the cache from disk
+    /// Loads the `ResponseCache` instance from disk.
     pub fn load() -> Result<ResponseCache> {
         let mut file = File::open(get_cache_path())?;
         let mut contents = String::new();
@@ -64,14 +73,20 @@ impl ResponseCache {
         Ok(deserialized)
     }
 
-    /// Delete the cache from disk
+    /// Deletes the `ResponseCache` instance from disk.
     pub fn delete(self) -> Result<()> {
         fs::remove_file(get_cache_path())?;
         Ok(())
     }
 }
 
-/// Returns the cache file path
+/// Returns the path to the cache file.
+///
+/// This function attempts to get the system's cache directory using the `BaseDirs` struct.
+/// If the cache directory doesn't exist, it attempts to create it.
+/// If it can't create the cache directory, it falls back to the home directory.
+/// If it can't get the home directory, it falls back to the current directory.
+/// The cache file is named ".lookupcache".
 pub fn get_cache_path() -> String {
     if let Some(base_dirs) = BaseDirs::new() {
         let mut dir = base_dirs.cache_dir();
