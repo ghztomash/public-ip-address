114e066dd98e88c1d88ca1b4d0a450c5d85321ce
Tomash Ghz <ghz.tomash@hotmail.com>
2024-04-03 01:29:19 +0200

    doc: add example using provider directtly

---
+++ examples/provider.rs
@@ -0,0 +1,15 @@
+use public_ip_address::lookup::{
+    ipwhois::{IpWhoIs, IpWhoIsResponse},
+    Provider,
+};
+use std::error::Error;
+
+/// This example demonstrates how to use the IpWhoIs provider directly
+/// and get access to the provider specific response.
+fn main() -> Result<(), Box<dyn Error>> {
+    let provider = IpWhoIs;
+    let response = provider.make_api_request()?;
+    let result = IpWhoIsResponse::parse(response)?;
+    println!("{:#?}", result);
+    Ok(())
+}
