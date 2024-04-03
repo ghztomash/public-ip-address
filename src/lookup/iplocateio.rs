e1d9a1f240a9a4c3d2386258ee8a6515abd360e8
Tomash Ghz <ghz.tomash@hotmail.com>
2024-04-03 17:00:24 +0200

    feat: add iplocate.io provider

---
+++ src/lookup/iplocateio.rs
@@ -0,0 +1,118 @@
+//! <https://iplocate.io> lookup provider
+
+use super::Result;
+use crate::{
+    lookup::{LookupProvider, Provider},
+    LookupResponse,
+};
+use serde::{Deserialize, Serialize};
+
+// https://iplocate.docs.apiary.io/
+#[derive(Serialize, Deserialize, Debug)]
+pub struct IpLocateIoResponse {
+    ip: String,
+    country: Option<String>,
+    country_code: Option<String>,
+    is_eu: Option<bool>,
+    city: Option<String>,
+    continent: Option<String>,
+    latitude: Option<f64>,
+    longitude: Option<f64>,
+    time_zone: Option<String>,
+    postal_code: Option<String>,
+    subdivision: Option<String>,
+    org: Option<String>,
+    asn: Option<String>,
+    threat: Option<Threat>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Threat {
+    is_proxy: Option<bool>,
+}
+
+impl IpLocateIoResponse {
+    pub fn parse(input: String) -> Result<IpLocateIoResponse> {
+        let deserialized: IpLocateIoResponse = serde_json::from_str(&input)?;
+        Ok(deserialized)
+    }
+
+    pub fn into_response(self) -> LookupResponse {
+        let mut response = LookupResponse::new(self.ip, LookupProvider::IpLocateIo);
+        response.country = self.country;
+        response.continent = self.continent;
+        response.country_code = self.country_code;
+        response.region = self.subdivision;
+        response.postal_code = self.postal_code;
+        response.city = self.city;
+        response.latitude = self.latitude;
+        response.longitude = self.longitude;
+        response.time_zone = self.time_zone;
+        response.asn_org = self.org;
+        response.asn = self.asn;
+        if let Some(threat) = self.threat {
+            response.proxy = threat.is_proxy;
+        }
+        response
+    }
+}
+
+pub struct IpLocateIo;
+impl Provider for IpLocateIo {
+    fn make_api_request(&self) -> Result<String> {
+        let client = reqwest::blocking::Client::new();
+        let response = client.get("https://www.iplocate.io/api/lookup/").send();
+        super::handle_response(response)
+    }
+
+    fn parse_reply(&self, json: String) -> Result<LookupResponse> {
+        let response = IpLocateIoResponse::parse(json)?;
+        Ok(response.into_response())
+    }
+
+    fn get_type(&self) -> LookupProvider {
+        LookupProvider::IpLocateIo
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const TEST_INPUT: &str = r#"
+{
+  "asn": "AS6185",
+  "city": "Cupertino",
+  "continent": "North America",
+  "country": "United States",
+  "country_code": "US",
+  "ip": "1.1.1.1",
+  "org": "Apple Inc.",
+  "latitude": 37.3042,
+  "longitude": -122.0946,
+  "postal_code": "95014",
+  "subdivision": "California",
+  "time_zone": "America/Los_Angeles"
+}
+"#;
+
+    #[test]
+    #[ignore]
+    fn test_request() {
+        let service = Box::new(IpLocateIo);
+        let result = service.make_api_request();
+        assert!(result.is_ok(), "Failed getting result {:#?}", result);
+        let result = result.unwrap();
+        assert!(!result.is_empty(), "Result is empty");
+        println!("IpLocateIo: {:#?}", result);
+        let response = IpLocateIoResponse::parse(result);
+        assert!(response.is_ok(), "Failed parsing response {:#?}", response);
+    }
+
+    #[test]
+    fn test_parse() {
+        let response = IpLocateIoResponse::parse(TEST_INPUT.to_string()).unwrap();
+        assert_eq!(response.ip, "1.1.1.1", "IP address not matching");
+        let lookup = response.into_response();
+        assert_eq!(lookup.ip, "1.1.1.1", "IP address not matching");
+    }
+}
